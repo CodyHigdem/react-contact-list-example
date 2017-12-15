8a88c994cf39d25a1654cc571c5fd0a7ccdd8b5f
Cody <meanderingmonarch@gmail.com>
2017-12-15 10:25:08 -0600

    stack navigator example

--- app/config/router.js
+++ app/config/router.js
@@ -1,7 +1,11 @@
-import { StackNavigator } from 'react-navigation';
+import React from 'react';
+import { StackNavigator, TabNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import Contacts from '../screens/Contacts';
 import Details from '../screens/Details';
+import NewContact from '../screens/NewContact';
+import Me from '../screens/Me';
 
 import { capitalizeFirstLetter } from '../helpers/string';
 
@@ -25,4 +29,44 @@ export const ContactsStack = StackNavigator({
     },
 });
 
-//this.props.navigation.navigate('Details')
+export const NewContactStack = StackNavigator({
+    NewContact: {
+        screen: NewContact,
+        navigationOptions:{
+            title: 'New Contact',
+        }
+    }
+});
+
+export const MeStack = StackNavigator({
+    Me: {
+        screen: Me,
+        navigationOptions:{
+            title: 'Me',
+        }
+    }
+})
+
+export const Tabs = TabNavigator({
+    Contacts: {
+        screen: ContactsStack,
+        navigationOptions: {
+            tabBarLabel: "Contacts",
+            tabBarIcon: ({tintColor})=> <Icon name="ios-list" size={35} color={tintColor}/>
+        }
+    },
+    NewContact: {
+        screen: NewContactStack,
+        navigationOptions: {
+            tabBarLabel: "New Contact",
+            tabBarIcon: ({tintColor})=> <Icon name="ios-add" size={35} color={tintColor}/>
+        }
+    },
+    Me: {
+        screen: MeStack,
+        navigationOptions: {
+            tabBarLabel: "Me",
+            tabBarIcon: ({tintColor})=> <Icon name="ios-contact" size={35} color={tintColor}/>
+        }
+    }
+});
