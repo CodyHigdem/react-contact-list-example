4dbc264deedc820fe0e2196233cadf40c8257238
Cody <meanderingmonarch@gmail.com>
2017-12-15 08:52:01 -0600

    first push

---
+++ app/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ContactsStack } from './config/router.js';
+
+const App = () => {
+    return <ContactsStack/>
+}
+
+export default App;
---
+++ index.android.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+  } from 'react-native';
+
+import App from './app/index';
+
+AppRegistry.registerComponent('YFRNApp', () => App);
